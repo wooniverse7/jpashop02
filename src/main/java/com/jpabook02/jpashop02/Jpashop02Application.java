0ec5684056edf21347ae17621a92fc817feeedc2
우성일 <wooniverse7@gmail.com>
2024-02-01 21:56:25 +0900

    경로에 한글명 없앨 것

--- src/main/java/com/jpabook02/jpashop02/Jpashop02Application.java
+++ src/main/java/com/jpabook02/jpashop02/Jpashop02Application.java
@@ -8,6 +8,11 @@ public class Jpashop02Application {
 
 	public static void main(String[] args) {
 
+		Hello hello = new Hello();
+		hello.setData("hello");
+		String data = hello.getData();
+		System.out.println("data = " + data);
+
 		SpringApplication.run(Jpashop02Application.class, args);
 	}
 
